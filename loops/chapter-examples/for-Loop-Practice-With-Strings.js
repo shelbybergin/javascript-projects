71c9f20c7401819a619fdba590366e11ecbecc00
ShelbyBergin <shelbyjb23@gmail.com>
2024-01-20 12:27:43 -0600

    loop practice with strings

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
-
+let myName = "Shelby";
 
 // Write a for loop that prints each character in your name on a different line.
+
+for (let i = 0; i < myName.length; i++) {
+    console.log(myName[i])
+}
